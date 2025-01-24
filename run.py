74e284e17be8f20ef0e588952741ce18a24f0506
Gabriela Paredes <paredesgab@gmail.com>
2025-01-24 01:00:15 +0000

    Wire up APIs and confirm access to my-finances google sheet

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+#Create constant variables
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('my_finances')
+
+#Access my-finances worksheet
+income = SHEET.worksheet('income')
+expenses = SHEET.worksheet("expenses")
+
+income_data = income.get_all_values()
+expenses_data = expenses.get_all_values()
+
+pprint(income_data) #output: a list of rows with string values
+pprint(expenses_data) #output: a list of rows with string values
